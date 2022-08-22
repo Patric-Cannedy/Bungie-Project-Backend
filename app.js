10afb62a33d522be9212989777655e65d837c630
Patric <paca7545@colorado.edu>
2022-08-22 08:16:36 -0700

    heroku

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(cookieParser());
 app.use('/Bungie-Project', manifest);
-// app.use(cors({
-//     origin: '*',
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200,
-// }));
+app.use(cors({
+    origin: '*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}));
 
 
 app.listen(port, host, () => console.log('server is up'));
