90c87f935750b4259deb36a73b6148660638b7cf
Patric <paca7545@colorado.edu>
2022-08-22 14:39:26 -0700

    routes updated for heroku

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.get("/", (req, res) => {
     res.send("Server Page");
  });
 let corsOptions= {
-    origin: 'http://localhost:5000',
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true,
     optionSuccessStatus:200,
 }; 
 app.use(cors());
