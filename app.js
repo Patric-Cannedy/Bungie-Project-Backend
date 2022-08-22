841fa6453e771ee31316ce446d85a5356bd903b5
Patric <paca7545@colorado.edu>
2022-08-22 10:59:57 -0700

    heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ let corsOptions= {
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 }; 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(cookieParser());
