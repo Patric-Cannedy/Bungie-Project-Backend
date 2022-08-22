e5257b9a48417eeb4ea8dd3f1cfe7e3c8f24dbf4
Patric <paca7545@colorado.edu>
2022-08-22 11:13:37 -0700

    heroku

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let corsOptions= {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(cookieParser());
-app.use('/Bungie-Project', manifest);
+app.use('/', manifest);
 
 
 
